77f820e140134224d53caef8a2b7318786e12452
Andrés Fernández <andresavena19@gmail.com>
2022-02-17 11:20:30 -0300

    feat: updated test helpers

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,8 @@
 import { test } from "matchstick-as";
 import {
 	testCallDataReplacement_0x4fece400c0d3db0937162ab44bab34445626ecfe_1626,
-	testCallDataReplacement_0xdceaf1652a131f32a821468dc03a92df0edd86ea_2fb1796
+	testCallDataReplacement_0xdceaf1652a131f32a821468dc03a92df0edd86ea_2fb1796,
+	testDecoderUpgrade
 } from "./callDataReplacement.test"
 
 
@@ -14,6 +15,10 @@ function runTests(): void {
 	test(
 		"testCallDataReplacement_0xdceaf1652a131f32a821468dc03a92df0edd86ea_2fb1796",
 		testCallDataReplacement_0xdceaf1652a131f32a821468dc03a92df0edd86ea_2fb1796
+	)
+	test(
+		"testDecoderUpgrade",
+		testDecoderUpgrade
 	)
 }
 runTests()
