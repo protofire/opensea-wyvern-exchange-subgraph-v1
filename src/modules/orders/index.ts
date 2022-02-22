bdf2e256d4b1798519c71fb2066bd59dedd112b1
Andrés Fernández <andresavena19@gmail.com>
2022-02-22 01:48:42 -0300

    feat: removed logs and added order struct comments

--- src/modules/orders/index.ts
+++ src/modules/orders/index.ts
@@ -70,25 +70,25 @@ export namespace orders {
 
 		export function getFeeMethod(feeMethod: i32): string {
 			let key = shared.helpers.i32ToString(feeMethod)
-			log.info("getFeeMethod said: {}", [key])
+			// log.info("getFeeMethod said: {}", [key])
 			return shared.helpers.getPropById(key, constants.getFeeTypes())
 		}
 
 		export function getSaleKind(kind: i32): string {
 			let key = shared.helpers.i32ToString(kind)
-			log.info("getSaleKind said: {}", [key])
+			// log.info("getSaleKind said: {}", [key])
 			return shared.helpers.getPropById(key, constants.getSaleKinds())
 		}
 
 		export function getHowToCall(call: i32): string {
 			let callAsString = shared.helpers.i32ToString(call)
-			log.info("getHowToCall said: {}", [callAsString])
+			// log.info("getHowToCall said: {}", [callAsString])
 			return shared.helpers.getPropById(callAsString, constants.getOrderCalls())
 		}
 
 		export function getOrderSide(side: i32): string {
 			let sideAsString = shared.helpers.i32ToString(side)
-			log.info("getOrderSide said: {}", [sideAsString])
+			// log.info("getOrderSide said: {}", [sideAsString])
 			return shared.helpers.getPropById(sideAsString, constants.getOrderSides())
 		}
 	}
@@ -112,53 +112,54 @@ export namespace orders {
 		return entity as Order
 	}
 	/*
-	 struct Order {
-			// Exchange address, intended as a versioning mechanism.
-			address exchange;
-			// Order maker address.
-			address maker;
-			// Order taker address, if specified.
-			address taker;
-			// Maker relayer fee of the order, unused for taker order.
-			uint makerRelayerFee;
-			// Taker relayer fee of the order, or maximum taker fee for a taker order.
-			uint takerRelayerFee;
-			// Maker protocol fee of the order, unused for taker order.
-			uint makerProtocolFee;
-			// Taker protocol fee of the order, or maximum taker fee for a taker order.
-			uint takerProtocolFee;
-			// Order fee recipient or zero address for taker order.
-			address feeRecipient;
-			// Fee method (protocol token or split fee).
-			FeeMethod feeMethod;
-			// Side (buy/sell).
-			SaleKindInterface.Side side;
-			// Kind of sale.
-			SaleKindInterface.SaleKind saleKind;
-			// Target.
-			address target;
-			// HowToCall.
-			AuthenticatedProxy.HowToCall howToCall;
-			// Calldata.
-			bytes calldata;
-			// Calldata replacement pattern, or an empty byte array for no replacement.
-			bytes replacementPattern;
-			// Static call target, zero-address for no static call.
-			address staticTarget;
-			// Static call extra data.
-			bytes staticExtradata;
-			// Token used to pay for the order, or the zero-address as a sentinel value for Ether.
-			address paymentToken;
-			// Base price of the order (in paymentTokens).
-			uint basePrice;
-			// Auction extra parameter - minimum bid increment for English auctions, starting/ending price difference.
-			uint extra;
-			// Listing timestamp.
-			uint listingTime;
-			// Expiration timestamp - 0 for no expiry.
-			uint expirationTime;
-			// Order salt, used to prevent duplicate hashes.
-			uint salt;
+		* struct Order {
+			* // Exchange address, intended as a versioning mechanism.
+			* address exchange;
+			* // Order maker address.
+			* address maker;
+			* // Order taker address, if specified.
+			* address taker;
+			* // Maker relayer fee of the order, unused for taker order.
+			* uint makerRelayerFee;
+			* // Taker relayer fee of the order, or maximum taker fee for a taker order.
+			* uint takerRelayerFee;
+			* // Maker protocol fee of the order, unused for taker order.
+			* uint makerProtocolFee;
+			* // Taker protocol fee of the order, or maximum taker fee for a taker order.
+			* uint takerProtocolFee;
+			* // Order fee recipient or zero address for taker order.
+			* address feeRecipient;
+			* // Fee method (protocol token or split fee).
+			* FeeMethod feeMethod;
+			* // Side (buy/sell).
+			* SaleKindInterface.Side side;
+			* // Kind of sale.
+			* SaleKindInterface.SaleKind saleKind;
+			* // Target.
+			* address target;
+			* // HowToCall.
+			* AuthenticatedProxy.HowToCall howToCall;
+			* // Calldata.
+			* bytes calldata;
+			* // Calldata replacement pattern, or an empty byte array for no replacement.
+			* bytes replacementPattern;
+			* // Static call target, zero-address for no static call.
+			* address staticTarget;
+			* // Static call extra data.
+			* bytes staticExtradata;
+			* // Token used to pay for the order, or the zero-address as a sentinel value for Ether.
+			* address paymentToken;
+			* // Base price of the order (in paymentTokens).
+			* uint basePrice;
+			* // Auction extra parameter - minimum bid increment for English auctions, starting/ending price difference.
+			* uint extra;
+			* // Listing timestamp.
+			* uint listingTime;
+			* // Expiration timestamp - 0 for no expiry.
+			* uint expirationTime;
+			* // Order salt, used to prevent duplicate hashes.
+			* uint salt;
+		}
 	*/
 	export function handleOrder(
 		exchange: Address,
